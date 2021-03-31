b89118c30a6145a024ecee04d336b6a9bbda2c37
Aliaksandra Los <aliaksandra_los@epam.com>
2021-03-31 16:35:20 +0300

    add project

---
+++ api_micro/api/api.py
@@ -0,0 +1,15 @@
+import requests
+from flask import Flask
+PORT = "8000"
+app = Flask(__name__)
+
+
+@app.route('/api/<name>')
+def get_info_btc(name):
+    req = requests.get(f'https://api.coingecko.com/api/v3/coins/{name}').json()
+    price_usd = req['market_data']['current_price']['usd']
+    db_req = requests.get(f'http://127.0.0.1:5000/api/update/{name}/{price_usd}')
+    return f'{name}: {price_usd}'
+
+
+app.run(port=PORT)
